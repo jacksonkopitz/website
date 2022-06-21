f827eed50ecafe836e145657b7ca96a139a5a1f4
Jackson Kopitz <jsk363@cornell.edu>
2022-06-20 23:58:01 -0700

    add github pages stuff

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,6 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
-        {/* <Route exact path="/success" component={Success} />
-        <Route exact path="/home" component={Llama} />
-        <Route exact path="/admin" component={Admin} />
-        <Route exact path="/add-posting" component={NewAddPosting} />
-        <Route exact path="/posting/:id" component={DetailedPosting} /> */}
       </Switch>
     </BrowserRouter>
   );
